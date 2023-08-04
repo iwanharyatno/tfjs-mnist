ce873e77da39ab39c2a70423ed272304a2074340
iwanharyatno <iwnharry61@gmail.com>
2023-08-04 11:02:16 +0700

    using model from official tfjs-example

--- model.js
+++ model.js
@@ -2,19 +2,26 @@ import tf from '@tensorflow/tfjs';
 
 const model = tf.sequential();
 model.add(tf.layers.conv2d({
-  inputShape: [28, 28, 1],
+  inputShape: [IMAGE_H, IMAGE_W, 1],
   kernelSize: 3,
   filters: 16,
-  activation: 'relu' 
+  activation: 'relu'
 }));
+model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+
+model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+
+model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
 model.add(tf.layers.flatten({}));
-model.add(tf.layers.dense({ units: 16, activation: 'relu' }))
-model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+
+model.add(tf.layers.dense({units: 64, activation: 'relu'}));
+model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
 model.compile({
-  optimizer: 'adam',
+  optimizer: 'rmsprop',
   loss: 'categoricalCrossentropy',
-  metrics: 'accuracy',
+  metrics: ['accuracy'],
 });
 
 export default model;
